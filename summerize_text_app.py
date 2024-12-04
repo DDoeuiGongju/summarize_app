988640c76c13d5c314f50f9f8af7bdafc6fdcc6c
SOEUI KIM <58338918+DDoeuiGongju@users.noreply.github.com>
2024-12-04 16:07:27 +0900

    Update summerize_text_app.py

--- summerize_text_app.py
+++ summerize_text_app.py
@@ -4,10 +4,11 @@ from transformers import pipeline
 
 def askGpt(prompt):
     messages=[{"role": "user", "content": prompt}]    
-    # pipe = pipeline("text-generation", model="Saxo/Linkbricks-Horizon-AI-Korean-Gemma-2-sft-dpo-27B")
-    pipe = pipeline("summarization")
-    gptResponse = pipe(prompt)
-    return gptResponse[0]['summary_text']
+    pipe = pipeline("text-generation", model="Saxo/Linkbricks-Horizon-AI-Korean-Gemma-2-sft-dpo-27B")
+    gptResponse = pipe(messages)
+    # pipe = pipeline("summarization")
+    # gptResponse = pipe(prompt)
+    return gptResponse
     
 def main():
     st.set_page_config(page_title="요약 프로그램")
@@ -21,7 +22,16 @@ def main():
     
     text = st.text_area("요약할 글을 입력하세요")
     if st.button("요약"):
-        prompt = f'''{text}'''
+        prompt = f'''
+        **Instructions** :
+    - You are an expert assistant that summarizes text into **Korean language**.
+    - Your task is to summarize the **text** sentences in **Korean language**.
+    - Your summaries should include the following :
+        - Omit duplicate content, but increase the summary weight of duplicate content.
+        - Summarize by emphasizing concepts and arguments rather than case evidence.
+        - Summarize in 3 lines.
+        - Use the format of a bullet point.
+    -text : {text}'''
         st.info(askGpt(prompt))
 
 if __name__=="__main__":
