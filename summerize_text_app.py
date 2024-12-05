386084b53d3141b0fb553333ee8d09b8d64f6953
SOEUI KIM <58338918+DDoeuiGongju@users.noreply.github.com>
2024-12-05 17:06:23 +0900

    Update summerize_text_app.py

--- summerize_text_app.py
+++ summerize_text_app.py
@@ -33,16 +33,7 @@ def main():
     
     text = st.text_area("요약할 글을 입력하세요")
     if st.button("요약"):
-        prompt = f'''
-        **Instructions** :
-    - You are an expert assistant that summarizes text into **Korean language**.
-    - Your task is to summarize the **text** sentences in **Korean language**.
-    - Your summaries should include the following :
-        - Omit duplicate content, but increase the summary weight of duplicate content.
-        - Summarize by emphasizing concepts and arguments rather than case evidence.
-        - Summarize in 3 lines.
-        - Use the format of a bullet point.
-    -text : {text}'''
+        prompt = f'''{text}'''
         st.info(askGpt(prompt, pipe))
 
 if __name__=="__main__":
